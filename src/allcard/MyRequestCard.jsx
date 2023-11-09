4597c5babab60791639720ef7015d8f5a7308476
Riad123456789 <riad80717@gmail.com>
2023-11-09 19:11:26 +0600

    my all style are done

--- src/allcard/MyRequestCard.jsx
+++ src/allcard/MyRequestCard.jsx
@@ -46,37 +46,43 @@ const MyRequestCard = ({ myrequestData, refetch }) => {
 
 
     return (
-        <div>
-            <div className="card w-96 mx-auto bg-base-100 shadow-xl">
+        <div className='p-10'>
+            {/* <div className="card w-96 mx-auto bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="card-title"></h2>
                     <h2 className="card-title">{DonationMoney} tk</h2>
                     <h2 className="card-title">{PickupLocation}</h2>
                     <h2 className="card-title">{ExpiredDate} days</h2>
                     <h2 className="card-title">{DonatorEmail}</h2>
-                    {/* <h2 className="card-title">{RequesterEmail}</h2> */}
+                    <h2 className="card-title">{RequesterEmail}</h2>
                     <h2 className="card-title">{RequestDate}</h2>
                     <h2 className="card-title">{FoodStatus}</h2>
 
 
                   
                 </div>
-            </div>
+            </div> */}
 
 
 
 
 
             <div className="card lg:card-side bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+                {/* <figure><img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure> */}
                 <div className="card-body">
                     <h2 className="card-title">DonatorName: {DonatorName}</h2>
-                    <p>Click the button to listen on Spotiwhy app.</p>
+                    <p> <span className='text-lg font-semibold btn btn-xs '>Location</span> :{PickupLocation}</p>
+                    <p> <span className='text-lg font-semibold btn btn-xs ' >ExpiredDate </span>:{ExpiredDate} days</p>
+                    <p> <span className='text-lg font-semibold btn btn-xs '>RequestDate</span> :{RequestDate} </p>
+                    <p><span className='text-lg font-semibold btn btn-xs '> FoodStatus</span> :{FoodStatus} </p>
                     <div className="card-actions justify-end">
-                    <button onClick={() => handleCancel(RequesterEmail)} className='btn btn-primary'>Cancel Request</button>
+                        <button onClick={() => handleCancel(RequesterEmail)} className='btn btn-secondary'>Cancel Request</button>
                     </div>
                 </div>
             </div>
+
+
+
         </div>
     );
 };
